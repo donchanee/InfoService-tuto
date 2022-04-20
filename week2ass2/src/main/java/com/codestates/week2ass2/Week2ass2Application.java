f334978c688451b59e302412f61d8c5a8077c522
dongchan <dongchan@qarasoft.com>
2022-04-20 15:53:22 +0900

    week2ass2 commit

---
+++ week2ass2/src/main/java/com/codestates/week2ass2/Week2ass2Application.java
@@ -0,0 +1,13 @@
+package com.codestates.week2ass2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2ass2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2ass2Application.class, args);
+    }
+
+}
---
+++ week2ass2/src/test/java/com/codestates/week2ass2/Week2ass2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.week2ass2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2ass2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
